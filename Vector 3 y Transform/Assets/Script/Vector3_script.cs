d7eb0d2737c708c564eebef01258eaab1419d443
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-05-17 10:43:40 -0300

    Player Movement

---
+++ Vector 3 y Transform/Assets/Script/Vector3_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3_script : MonoBehaviour
+{
+    public Vector3 myVector;
+    public GameObject myObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myObject.transform.position = myVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
